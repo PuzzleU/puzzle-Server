77db42dae7ad3b50a2d2e1694d3371e1b8d9f807
sunghyun1356 <sunghyun1356@naver.com>
2024-01-10 13:10:35 +0900

    FEAT : ResponseUtils와 SuccessResponse 추ã가

---
+++ src/main/java/com/PuzzleU/Server/common/SuccessResponse.java
@@ -0,0 +1,25 @@
+package com.PuzzleU.Server.common;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class SuccessResponse {
+    private int status;
+    private String message;
+
+    @Builder
+    private SuccessResponse(int status, String message)
+    {
+        this.status = status;
+        this.message = message;
+    }
+    public static SuccessResponse of(HttpStatus status, String message)
+    {
+        return SuccessResponse.builder()
+                .status(status.value())
+                .message(message)
+                .build();
+    }
+}
