c6a777ee320f9d24a433a2aae6bfbc7c04803fc7
sunghyun1356 <sunghyun1356@naver.com>
2024-01-27 12:29:42 +0900

    [FEAT] #9 Enity ã패키지 추가

--- src/main/java/com/PuzzleU/Server/entity/apple/AppleIdTokenPayload.java
+++
@@ -1,9 +0,0 @@
-package com.PuzzleU.Server.entity.apple;
-
-import lombok.Getter;
-
-@Getter
-public class AppleIdTokenPayload {
-    private String sub; // id토큰의 고유 식별자
-    private String email;
-}
