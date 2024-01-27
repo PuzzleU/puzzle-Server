648f157f3561101c35435f3632d7cb51cdf7802c
13155a1 <ppy040@gmail.com>
2024-01-27 15:27:15 +0900

    [FEAT] Create UserPositionRelation Entity

--- src/main/java/com/PuzzleU/Server/entity/relations/UserPositionRelation.java
+++ src/main/java/com/PuzzleU/Server/entity/relations/UserPositionRelation.java
@@ -1,4 +1,32 @@
 package com.PuzzleU.Server.entity.relations;
 
+import com.PuzzleU.Server.entity.apply.Apply;
+import com.PuzzleU.Server.entity.position.Position;
+import com.PuzzleU.Server.entity.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "userPositionRelation")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserPositionRelation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long UserPositionRelationId;
+
+    // 의존 관계 매핑 (User)
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User User;
+
+    // 의존관계 매핑 (Position)
+    @ManyToOne
+    @JoinColumn(name = "positionId")
+    private Position Position;
 }
