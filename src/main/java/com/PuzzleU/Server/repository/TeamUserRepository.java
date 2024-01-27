21d8cab5c7a22a2538cc005c8208b59c10bd3a9c
13155a1 <ppy040@gmail.com>
2024-01-27 17:00:01 +0900

    [FEAT] Create TeamUserRepository

--- src/main/java/com/PuzzleU/Server/repository/TeamUserRepository.java
+++ src/main/java/com/PuzzleU/Server/repository/TeamUserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TeamUserRelationRepository extends JpaRepository<TeamUserRelation, Long> {
+public interface TeamUserRepository extends JpaRepository<TeamUserRelation, Long> {
 }
