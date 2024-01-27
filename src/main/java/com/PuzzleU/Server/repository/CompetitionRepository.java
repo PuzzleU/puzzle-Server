54d4547e8fd8127a57db680e83cde73ce48495f4
13155a1 <ppy040@gmail.com>
2024-01-27 16:41:57 +0900

    [FEAT] #29 Create CompetitionRepository

---
+++ src/main/java/com/PuzzleU/Server/repository/CompetitionRepository.java
@@ -0,0 +1,9 @@
+package com.PuzzleU.Server.repository;
+
+import com.PuzzleU.Server.entity.competition.Competition;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompetitionRepository extends JpaRepository<Competition, Long> {
+}
