6bed7cf6f6474cf2394eaed45601b82ca22f1ab8
13155a1 <ppy040@gmail.com>
2024-01-27 15:05:06 +0900

    [FEAT] #9 Create Major Entity

--- src/main/java/com/PuzzleU/Server/entity/major/Major.java
+++ src/main/java/com/PuzzleU/Server/entity/major/Major.java
@@ -1,4 +1,28 @@
 package com.PuzzleU.Server.entity.major;
 
+import com.PuzzleU.Server.entity.position.Position;
+import com.PuzzleU.Server.entity.university.University;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "major")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Major {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long MajorId;
+
+    private String MajorName; // 전공 이름
+
+    // 의존 관계 매핑 (University)
+    @ManyToOne
+    @JoinColumn(name = "UniversityId")
+    private University University;
 }
