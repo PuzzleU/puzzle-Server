f0feed7b70caed40b9a46d6599f214a950e804a6
sunghyun1356 <sunghyun1356@naver.com>
2024-01-29 16:32:58 +0900

    [FEAT] #9 Competition DATE TemporalType.TIMESTAMP refactoring

--- src/main/java/com/PuzzleU/Server/entity/Competition.java
+++ src/main/java/com/PuzzleU/Server/entity/Competition.java
@@ -37,9 +37,11 @@ public class Competition extends BaseEntity {
     private String CompetitionAwards;
 
     @Column(name = "Competition_start", nullable = true)
+    @Temporal(value = TemporalType.TIMESTAMP)
     private Date CompetitionStart;
 
     @Column(name = "Competition_end", nullable = true)
+    @Temporal(value = TemporalType.TIMESTAMP)
     private Date CompetitionEnd;
 
     @Column(name = "Competition_content", nullable = true, length = 2000)
