a6c058a8299f2ce28bf04f91dc9b76630be7bce5
sunghyun1356 <sunghyun1356@naver.com>
2024-01-29 15:03:29 +0900

    [FEAT] #9 LIKE CLASS CHANGED TO HEART CLASS (SQL ERROR)

--- src/main/java/com/PuzzleU/Server/repository/HeartRepository.java
+++ src/main/java/com/PuzzleU/Server/repository/HeartRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LikeRepository extends JpaRepository<Heart, Long> {
+public interface HeartRepository extends JpaRepository<Heart, Long> {
 }
