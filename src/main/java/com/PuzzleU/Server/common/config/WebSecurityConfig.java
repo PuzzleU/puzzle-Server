1653902271ebaacc5084b3a8db9bbe3ae133955d
sunghyun1356 <sunghyun1356@naver.com>
2024-02-22 16:05:50 +0900

    [FEAT] EnableScheduling Add

--- src/main/java/com/PuzzleU/Server/common/config/WebSecurityConfig.java
+++ src/main/java/com/PuzzleU/Server/common/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,6 +24,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @RequiredArgsConstructor
 @EnableMethodSecurity
+@EnableScheduling
 public class WebSecurityConfig {
 
     private final JwtUtil jwtUtil; // Jwt를 사용할 것이므로 선언
